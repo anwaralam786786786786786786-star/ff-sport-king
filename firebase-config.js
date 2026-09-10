8a115db617a8fccaf4cb1fadfb30e6ca85de54f1
anwaralam786786786786786786-star <anwaralam786786786786786786@gmail.com>
2026-09-10 22:24:28 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDslrECNNJNo19MYQf6A6vGgNLmype-G00",
+  authDomain: "ff-sport-king.firebaseapp.com",
+  databaseURL: "https://ff-sport-king-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ff-sport-king",
+  storageBucket: "ff-sport-king.firebasestorage.app",
+  messagingSenderId: "157253393114",
+  appId: "1:157253393114:web:a6b2d80ba5fc173a2123e3",
+  measurementId: "G-TV16QYZ0H5"
+};
